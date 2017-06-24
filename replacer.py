ff8aa90eb629eb89950358ee2e47b09e0ab32a7b
Dimitri Merejkowsky <d.merej@gmail.com>
2017-06-24 18:30:01 +0200

    remove --no-filter option
    I fail to see a use case for replacing stuff in anything that is
    already ignored by default

--- replacer.py
+++ replacer.py
@@ -92,11 +92,10 @@ def recurse_file(args, directory, action):
         if args.skip_hidden and f.startswith("."):
             continue
         filter_out = False
-        if not args.no_filter:
-            for fo in FILTER_OUT:
-                if fnmatch.fnmatch(f, fo):
-                    filter_out = True
-                    break
+        for fo in FILTER_OUT:
+            if fnmatch.fnmatch(f, fo):
+                filter_out = True
+                break
         if filter_out:
             continue
         if args.includes:
@@ -212,8 +211,6 @@ def main(args=None):
                         "Use this if you know what you are doing...")
     parser.add_argument("--include", dest="includes", action="append",
                         help="Only replace in files matching theses patterns")
-    parser.add_argument("--no-filter", action="store_true", dest="no_filter",
-                        help="Do not skip files that match the filter")
     parser.add_argument("-d", "--debug", action="store_true", dest="debug",
                         help="Enable debug output")
     parser.add_argument("--backup",
@@ -238,7 +235,6 @@ def main(args=None):
 
     parser.set_defaults(
         skip_hidden=True,
-        no_filter=False,
         backup=False,
         go=False,
         color=True,
